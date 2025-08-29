86fa8b4ae0d38e17c5464661f7df3edc6a158943
Deepanshu nayak <147653394+psychowitz500@users.noreply.github.com>
2025-08-30 04:55:16 +0530

    Fix: corrected component import case sensitivity

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import HeroSection from './components/HeroSection';
 import FeatureSection from './components/FeatureSection';
 import Workflow from './components/Workflow'; 
